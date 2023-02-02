f712d857ee80eccce53a77f3d78146e1904efe64
Katarzyna Jaszczur <34289@student.anstar.edu.pl>
2023-02-02 20:59:55 +0100

    Laboratorium 6

---
+++ laboratorium6/app/config.js
@@ -0,0 +1,8 @@
+const config = {
+    port: process.env.PORT || 3000
+   };
+   
+module.exports = {
+    config: config,
+    databaseUrl: "mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
